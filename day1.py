827e08f5087a8ab7c07c4c9bc36cf8386b093421
UnnimayaRam <unnimayaram1@gmail.com>
2022-07-02 22:38:54 +0530

    first commite

---
+++ day1.py
@@ -0,0 +1,99 @@
+# print("hello world")
+# arithmatic operation
+# a=5
+# b=3
+# c=a+b
+# print(c)
+# a=5
+# b=3
+# c=a-b
+# print(c)
+# a=5
+# b=3
+# c=a/b
+# print(c)
+# a=5
+# b=3
+# c=a%b
+# print(c)
+# a=5
+# b=3
+# c=a**b
+# print(c)
+#
+# a=5
+# b=3
+# c=a//b
+# print(c)
+# a=5
+# b=4
+# c=a*b
+# print(c)
+
+# a=5
+# a=a+5
+# print(a)
+
+# a=6
+# a=a-4
+# print(a)
+# logical operation
+# a=True
+# b=False
+# print(a and b)
+# print(a or b)
+# a=int(input("enter the first number:"))
+# b=int(input("enter the second number:"))
+# result=(a**2)+(b**2)+(2*a*b)
+# print(result)
+# if elif else condition
+# a=int(input("enter the first number:"))
+# b=int(input("enter the second number:"))
+# if a>b:
+#     print(a)
+# elif a<b:
+#     print(b)
+# else:
+#     print("invalid")
+
+# name entering
+# a="abc"
+# b="def"
+# name1=input("enter the first name:")
+# if name1==a:
+#     print("correct")
+#     name2=input("second name")
+#     if name2==b:
+#         print("correct")
+#     else:
+#         print("second name is wrong ")
+# else:
+#     print("wrong")
+
+ # largest three number
+
+# a=int(input("enter the first number:"))
+# b=int(input("enter the second number:"))
+# c=int(input("enter the third number:"))
+# if a>b:
+#      if a>c:
+#          print("a is greatest",a)
+#      else:
+#          print("c is greatest",c)
+# elif b>a:
+#      if b>c:
+#          print("b is gratest",b)
+#      else:
+#
+#          print("c is greatest",c)
+
+
+# calculator
+
+
+
+
+
+
+
+
